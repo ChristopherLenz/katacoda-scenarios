5ec42ff425d87e95688d0b68c326b152ad814746
ChristopherLenz <info@christopher-lenz.de>
2021-12-22 15:00:29 +0100

    verification

--- namespaces/1_verify.sh
+++ namespaces/1_verify.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 grep ns1 -ie 'challenge-created-ns-4711'
-
-# prepare task 4
-if [ ! -f /opt/4_deploy.log ]; then
-    kubectl run nginx-challenge --image=nginx -n challenge-created-ns-0817 > /opt/4_deploy.log
-fi
--- namespaces/4_verify.sh
+++ namespaces/4_verify.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# prepare task 4
+if [ ! -f /opt/4_deploy.log ]; then
+    kubectl run nginx-challenge --image=nginx -n challenge-created-ns-0817 > /opt/4_deploy.log
+fi
+
 grep ns4 -ie 'challenge-created-ns-0817'
 [[ -f /root/ns4 ]] && [[ $(cat /root/ns4 | wc -l) -eq 1 ]] && echo done || exit 1
